659b1cfa35b8684a74d005cf654920f4581cf932
dae-wook <67859264+dae-wook@users.noreply.github.com>
2023-01-27 13:55:50 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,15 @@
+class Solution {
+        public static int solution(int hp) {
+        int answer = 0;
+        while(hp >= 5) {
+            hp -= 5;
+            answer++;
+        }
+        while(hp >= 3) {
+            hp -= 3;
+            answer++;
+        }
+        answer += hp;
+        return answer;
+    }
+}
