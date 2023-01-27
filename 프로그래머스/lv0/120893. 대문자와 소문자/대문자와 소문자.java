054fcdd518c8b848021a6a8dd9ebf33aa98cb7c4
dae-wook <67859264+dae-wook@users.noreply.github.com>
2023-01-27 20:58:54 +0900

    [level 0] Title: 대문자와 소문자, Time: 14.40 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(char c : my_string.toCharArray()) {
+            if (c > 96) answer += (char)(c-32);
+            else answer += (char)(c+32);
+        }
+        return answer;
+    }
+}
