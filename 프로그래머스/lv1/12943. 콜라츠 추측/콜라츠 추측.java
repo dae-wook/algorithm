660e44bad34aa71e66cc333fa4cf801418b1d236
dae-wook <67859264+dae-wook@users.noreply.github.com>
2023-01-28 18:52:47 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int num) {
-        long l = num;
         if(num == 1) return 0;
+        long l = num;
         int answer = 0;
         while(l != 1) {
             l = l % 2 == 0 ? l / 2 : l * 3 + 1;
