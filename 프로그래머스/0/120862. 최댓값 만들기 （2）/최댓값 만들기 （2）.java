41760ede8e9f95bea0d8fb4b21c35f40e05c89e5
dae-wook <67859264+dae-wook@users.noreply.github.com>
2025-06-08 12:06:12 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.27 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = -1000000000;
+        
+        for(int i = 0; i < numbers.length - 1; i++) {
+            for(int j = 0; j < numbers.length; j++) {
+                if(i == j) continue;
+                
+                int multi = numbers[i] * numbers[j];
+                if(answer <= multi) {
+                    answer = multi;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
