e368c11a9e1ec469716d175eb209992b8b210055
dae-wook <67859264+dae-wook@users.noreply.github.com>
2025-06-08 13:27:48 +0900

    [level 0] Title: 숫자 찾기, Time: 8.98 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        
-        StringBuilder sb = new StringBuilder(String.valueOf(num));
-        if(sb.indexOf(k + "") == -1) return -1;
-        return sb.indexOf(k + "") + 1;
+        return ("." + String.valueOf(num)).indexOf(k + "");
     }
 }
