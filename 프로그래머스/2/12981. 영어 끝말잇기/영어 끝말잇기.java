048f160e99a112fc5c8f17d0d54e5d9e1477a9af
dae-wook <67859264+dae-wook@users.noreply.github.com>
2025-06-23 18:03:21 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,16 +3,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = {0, 0};
-        Map<String,Integer> wordMap = new HashMap<>();
-        wordMap.put(words[0], 1);
+        Set<String> wordSet = new HashSet<>();
+        wordSet.add(words[0]);
         for(int i = 1; i < words.length; i++) {
             char lastLetter = words[i - 1].charAt(words[i - 1].length() - 1);
-            if(wordMap.get(words[i]) != null || words[i].charAt(0) != lastLetter) {
+            if(wordSet.contains(words[i]) || words[i].charAt(0) != lastLetter) {
                 answer[0] = i % n + 1;
                 answer[1] = i / n + 1;
                 break;
             }
-            wordMap.put(words[i], 1);
+            wordSet.add(words[i]);
         }
         return answer;
     }
