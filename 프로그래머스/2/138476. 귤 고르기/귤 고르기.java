010fca177023e4ece751876955654131e7d73ddf
dae-wook <67859264+dae-wook@users.noreply.github.com>
2025-06-23 15:54:51 +0900

    [level 2] Title: 귤 고르기, Time: 63.78 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        Map<Integer,Integer> map = new HashMap<>();
+        
+        for(int num : tangerine) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            list.add(entry.getValue());
+        }
+        
+        Collections.sort(list, Collections.reverseOrder());
+        int count = 0;
+        
+        for(int i = 0; i < list.size(); i++) {
+            if(count + list.get(i) >= k) {
+                return i + 1;
+            }
+            count += list.get(i);
+        }
+        
+        return answer;
+    }
+}
